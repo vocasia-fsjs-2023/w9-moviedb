dbd0d1425026092c46c3b56c6848c65ddefbdf48
nishly <adhipamungkaswijayadi@gmail.com>
2023-10-16 20:15:05 +0700

    Done Task 9

--- models/index.js
+++ models/index.js
@@ -1,43 +1,26 @@
-'use strict';
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const sequelize = new Sequelize(config);
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const Movie = require("./movie")(sequelize, Sequelize.DataTypes);
+const Review = require("./review")(sequelize, Sequelize.DataTypes);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+Movie.hasMany(Review, {
+  foreignKey: "MovieId",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+Review.belongsTo(Movie, {
+  foreignKey: "MovieId",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
 
-module.exports = db;
+module.exports = {
+  sequelize,
+  Movie,
+  Review,
+};
