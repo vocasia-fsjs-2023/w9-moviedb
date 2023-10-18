e585e008cc3066612da8f695de29b5231f0d44ed
yunanda0050 <142362343+yunanda0050@users.noreply.github.com>
2023-10-18 20:21:09 +0700

    Add files via upload

---
+++ migrations/20231014110939-create-review.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        validate: {
+          len: {
+            args: [1, 255],
+          },
+        },
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      rating: {
+        type: Sequelize.INTEGER,
+        validate: {
+          min: 0,
+          max:5,
+        },
+      },
+      movieId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Movies",
+          },
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reviews');
+  }
+};
