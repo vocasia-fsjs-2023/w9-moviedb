6676782d4bb8ae4dfbfc0aa7e20b02597dd1ab56
Andreas Sibua <93329612+tiandreassibua@users.noreply.github.com>
2023-10-18 23:23:37 +0700

    Update review migration

--- migrations/20231013072450-create-review.js
+++ migrations/20231013072450-create-review.js
@@ -1,56 +1,48 @@
-'use strict';
+"use strict";
+
+const { DataTypes } = require("sequelize");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Reviews', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        validate: {
-          len: {
-            args: [1, 255],
-          },
-        },
-      },
-      description: {
-        allowNull: false,
-        type: Sequelize.TEXT
-      },
-      rating: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        validate: {
-          min: 0,
-          max:5,
-        },
-      },
-      movieId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: {
-          model: {
-            tableName: "Movies",
-          },
-          key: "id",
-        },
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("Reviews", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            title: {
+                allowNull: false,
+                type: DataTypes.STRING,
+            },
+            description: {
+                allowNull: false,
+                type: DataTypes.TEXT,
+            },
+            rating: {
+                allowNull: false,
+                type: DataTypes.INTEGER,
+            },
+            movieId: {
+                allowNull: false,
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "Movies",
+                    key: "id",
+                },
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("Reviews");
+    },
 };
