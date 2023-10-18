3d99adf234e15a09ea1db81bec99b584ce4cf2bf
zulfaapril25 <142382739+zulfaapril25@users.noreply.github.com>
2023-10-18 21:34:34 +0700

    update week 9
    update week 9

---
+++ controller/reviewController.js
@@ -0,0 +1,95 @@
+const { Review, Movie} = require('../models')
+
+//POST
+const createReview = async (req, res) => {
+    
+    try {
+        const {title, description, rating, movieId} = req.body;
+        const reviewBaru = await Review.create({
+            title: title,
+            description: description,
+            rating: rating,
+            movieId: movieId
+        });
+        const load = await reviewBaru.reload({
+            include: [
+                {
+                    model: Movie,
+                    as: 'movies'
+                }
+            ]
+        })
+
+        res.status(200).json(load);
+    }catch(error){
+        console.log(error, '<-- Error Create Review')
+       } 
+    }
+
+//GET DATA
+const getAllReview = async(req, res) => {
+   try{
+    const review = await Review.findAll({
+        attributes:['title', 'description', 'rating', 'movieId'],
+        include: [
+            {
+                model: Movie,
+                as: "movies",
+            }
+        ]
+    });
+    res.status(200).json({ review });
+    
+   }catch (error){
+    res.status(404).json({ message: 'review tidak ditemukan' });
+   }
+    }
+
+
+//PUT
+const updateDataReview = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, rating } = req.body;
+
+        const review = await Review.findByPk(id);
+        if (!review) {
+            return res.status(404).json({
+                 message: "Review tidak ditemukan" });
+        }
+
+        review.title = title || review.title;
+        review.description = description || review.description;
+        review.rating = rating || review.rating;
+
+        await review.save();
+
+        return res.status(200).json({
+            title: review.title,
+            description: review.description,
+            rating: review.rating,
+            updatedAt: review.updatedAt,
+        });
+    } catch(error) {
+        console.log(error, '<-- Error Update Data Review by ID')
+    }
+};
+
+//DELETE
+    const deleteReviews = async (req, res) => {
+        try{
+            const { id } = req.params
+            const review = await Review.findByPk(id)
+
+            if (!review) {
+                res.status(404).json({ message: 'review tidak ditemukan' });
+            }
+            review.destroy()
+            res.status(200).json({ message: `review dengan id ${id} berhasil dihapus` });
+
+        }catch(error){
+            console.log(error, '<-- Error Destroy review')
+        }
+    }
+
+module.exports = {createReview, getAllReview, updateDataReview, deleteReviews}
