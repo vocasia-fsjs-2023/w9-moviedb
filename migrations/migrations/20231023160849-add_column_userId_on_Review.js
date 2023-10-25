e52741b28797cfd6b5fb9175f75adbbd982ff248
zulfaapril25 <142382739+zulfaapril25@users.noreply.github.com>
2023-10-25 20:59:36 +0700

    update week 10
    mengumpulkan week 10

---
+++ migrations/migrations/20231023160849-add_column_userId_on_Review.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    queryInterface.addColumn(
+      'Reviews', 'userId', { 
+        type: Sequelize.INTEGER,
+        references: {
+        model: "Users",
+        key: "id",
+        },
+      });
+  },
+
+  async down (queryInterface, Sequelize) {
+   await queryInterface.removeColumn("Reviews", "userId");
+  },
+};
