a0a9d4b3fdbdae31b69f7045a08ce3376d1aab72
zulfaapril25 <142382739+zulfaapril25@users.noreply.github.com>
2023-10-25 21:03:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+
 const movie = require('./routes/movieRoutes.js')
 const review = require('./routes/reviewRoutes.js')
+const user = require('./routes/userRoutes.js')
 
-const { Movie } = require("./models");
-const { Review } = require("./models")
+const { Movie, Review, User } = require("./models");
 
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(movie)
 app.use(review)
+app.use(user)
 
 // Menjalankan server
 app.listen(port, () => {
